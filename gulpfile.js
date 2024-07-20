0c3525f240fe25c5ca0d429453bb4e8e1124665e
Oyama <oyamasscruz@gmail.com>
2024-07-20 16:59:56 -0300

    last one commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ const imagemin = require('gulp-imagemin');
 //to call the dependende you have to call the dependence as a function.
 function styles() {
     return gulp.src('./src/styles/*.scss') // rigth here you have to use gulp.src , so say where is the source(src) of document that will be modificate
-        .pipe(sass({outputStyles:'compressed'}))// rigth here you call the function of the dependence - sass --> compressed
+        .pipe(sass({
+            outputStyles:'compressed'
+        }).on('error', sass.logError))// rigth here you call the function of the dependence - sass --> compressed
         .pipe(gulp.dest('./dist/css')); // and at the final, you say the place of modification final;
 }
 
@@ -25,6 +27,6 @@ function scripts() {
 exports.default = gulp.parallel(styles, images, scripts) // to call all of dependeces you have to use gulp.parallel, to call as parallel tasks.
 
 exports.watch = function() { // this function it's to watch the modifications of documents that are on watch, as SASS archive, JS archive and images archives.
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/styles/*.scss', { igonreInitial: false }, gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', { igonreInitial: false }, gulp.parallel(scripts))
 }
